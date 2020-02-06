30605cb34f167a1b5b296fa97408143ff6e9142a
apis-tam <bogdanapis@gmail.com>
2020-02-06 10:37:49 +0200

    h

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,52 @@
-'use strict';
+"use strict";
 
-(function($){
-	$(document).ready(function() {
-		// Code
+(function($) {
+  $(document).ready(function() {
+    // Code
+    //slider
+    $(".uslider").slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      rows: 2,
+      slidesPerRow: 1,
 
-	});
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            rows: 2,
+            slidesPerRow: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            rows: 1,
+            arrows: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            rows: 1,
+            arrows: true,
+            dots: false
+          }
+        }
+      ]
+    });
+    //slider-end
+  });
 })(jQuery);
